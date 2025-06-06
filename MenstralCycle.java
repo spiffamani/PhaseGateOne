4e7644b4f875d2a4d6cfeccbd0a619babaec3772
spiffamani <cjdaveeds@gmail.com>
2025-06-06 18:10:17 +0100

    Menstral cycle program

---
+++ MenstralCycle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class MenstralCycle {
+
+public static String getusername(){
+Scanner input =  new Scanner(System.in);
+System.out.print("Enter your name :  ");
+
+String name = input.nextLine();
+return name;
