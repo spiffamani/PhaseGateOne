a091eee79cee7755808c4d82dcf11ce216234c1c
spiffamani <cjdaveeds@gmail.com>
2025-06-08 15:34:38 +0100

    Menstral cycle calculator

--- MenstralCycle.java
+++ MenstralCycle.java
@@ -1,9 +1,64 @@
+
+
+import java.time.LocalDate;
 import java.util.Scanner;
+
 public class MenstralCycle {
 
-public static String getusername(){
-Scanner input =  new Scanner(System.in);
-System.out.print("Enter your name :  ");
+    public static String getUsername() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        return input.nextLine();
+    }
 
-String name = input.nextLine();
-return name;
+    public static LocalDate lastMenstrualCycle() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your last menstrual cycle (yyyy-mm-dd): ");
+        String lastDateString = input.nextLine();
+        return LocalDate.parse(lastDateString);
+    }
+
+    public static LocalDate nextMenstrualCycle(LocalDate lastCycle, int dateInterval) {
+        return lastCycle.plusDays(dateInterval);
+    }
+
+    public static LocalDate lengthOfCycle(LocalDate lastCycle, int lengthInterval) {
+        return lastCycle.minusDays(lengthInterval);
+    }
+
+    public static void main(String[] args) {
+        String userName = getUsername();
+        LocalDate userLastMenstrualCycle = lastMenstrualCycle();
+
+        int dateInterval = 28;
+        int lengthInterval = 14;
+
+        LocalDate userNextMenstrualCycle = nextMenstrualCycle(userLastMenstrualCycle, dateInterval);
+        LocalDate userLengthOfCycle = lengthOfCycle(userLastMenstrualCycle, lengthInterval);
+
+        System.out.println("\nHello, " + userName + "!");
+        System.out.println("Your last menstrual cycle was on: " + userLastMenstrualCycle);
+        System.out.println("Your next menstrual cycle is expected on: " + userNextMenstrualCycle);
+        System.out.println("Your  cycle length calculation: " + userLengthOfCycle);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
