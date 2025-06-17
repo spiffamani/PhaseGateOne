e513ac1e73348c217d3deb47004aa21d363036db
spiffamani <cjdaveeds@gmail.com>
2025-06-17 17:45:07 +0100

    task five

---
+++ FireDrill/taskfive.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+public class taskfive {
+    public static void main(String[] args) {
+        for (int num = 4; num <= 10; num += 4) { 
+            for (int i = 0; i < 5; i++) { 
+                System.out.print(num);
+            }
+            System.out.println(); 
+        }
+    }
+}
